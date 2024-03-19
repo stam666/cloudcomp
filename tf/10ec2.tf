a2518c048b57f0412bcc60e4921c9ca280240c4d
stam <stamp.chayakorn@gmail.com>
2024-03-19 16:16:24 +0700

    Update environment variables and configuration settings in 10ec2.tf

--- tf/10ec2.tf
+++ tf/10ec2.tf
@@ -14,16 +14,6 @@ resource "aws_instance" "app" {
 
   user_data = <<-EOF
   #!/bin/bash
-  export DB_HOST=${aws_network_interface.db_app_eni.private_ip}
-  export DB_NAME=${var.database_name}
-  export DB_USER=${var.database_user}
-  export DB_PASS=${var.database_pass}
-  export WP_PUBLIC_IP=${aws_eip.app_eip.public_ip}
-  export WP_ADMIN_USER=${var.admin_user}
-  export WP_ADMIN_PASS=${var.admin_pass}
-  export BUCKET_NAME=${var.bucket_name}
-  export REGION=${var.region}
-
   sudo sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
   sudo apt-get update
   sudo apt-get install -y apache2 php8.1 libapache2-mod-php php8.1-mysql php8.1-curl php8.1-gd php8.1-mbstring php8.1-xml php8.1-xmlrpc php8.1-soap php8.1-intl php8.1-zip libapache2-mod-php
@@ -40,14 +30,14 @@ resource "aws_instance" "app" {
   sudo sed -i "s/database_name_here/${var.database_name}/" /var/www/html/wp-config.php
   sudo sed -i "s/username_here/${var.database_user}/" /var/www/html/wp-config.php
   sudo sed -i "s/password_here/${var.database_pass}/" /var/www/html/wp-config.php
-  sudo sed -i "s/localhost/$DB_HOST/" /var/www/html/wp-config.php
+  sudo sed -i "s/localhost/${aws_network_interface.db_app_eni.private_ip}/" /var/www/html/wp-config.php
 
   cat <<EOT > /tmp/wp-offload-media-settings.txt
   define('AS3CF_SETTINGS', serialize(array(
       'provider' => 'aws',
       'use-server-roles' => true,
-      'bucket' => '$BUCKET_NAME',
-      'region' => '$REGION',
+      'bucket' => ${var.bucket_name},
+      'region' => ${var.region},
       'copy-to-s3' => true,
       'serve-from-s3' => true,
   )));
@@ -55,14 +45,18 @@ resource "aws_instance" "app" {
 
   sudo sed -i "/define( 'WP_DEBUG', false );/r /tmp/wp-offload-media-settings.txt" /var/www/html/wp-config.php
   sudo sed -i '/<Directory /var/www/>/,/<\/Directory>/s/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
+  sudo sed -i 's/\(DirectoryIndex\s.*\)index\.html\(.*\)index\.php\(.*\)/\1index.php\2index.html\3/' /etc/apache2/mods-enabled/dir.conf
+  sudo sed -i 's/\(DirectoryIndex\s.*\)index\.html\(.*\)index\.php\(.*\)/\1index.php\2index.html\3/' /etc/apache2/mods-enabled/dir.conf
 
   sudo curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
   sudo chmod +x wp-cli.phar
   sudo mv wp-cli.phar /usr/local/bin/wp
-  sudo wp core install --path=/var/www/html --allow-root --url=$WP_PUBLIC_IP --title="CloudCompMidterm" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email="example@example.com" --skip-email
+  sudo wp core install --path=/var/www/html --allow-root --url=${aws_eip.app_eip.public_ip} --title=CloudCompMidterm --admin_user=${var.admin_user} --admin_password=${var.admin_pass} --admin_email=example@example.com --skip-email
   sudo wp plugin install amazon-s3-and-cloudfront --path=/var/www/html --allow-root --activate
   sudo systemctl restart apache2
   EOF
+
+  depends_on = [aws_network_interface.app_eni, aws_network_interface.db_app_eni, aws_instance.db]
   tags = {
     Name = "app-instance"
   }
